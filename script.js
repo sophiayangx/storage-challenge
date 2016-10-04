1c2236198cb46119656eff2271a84e51567626b3
sophiayangx <sopyang@nuevaschool.org>
2016-10-04 08:45:23 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ var input = $("#not-a-real-id")
 input.keydown(function(e) {	
   if (e.keyCode == "13") {
     var valueToSave = input.val();
-    storeValue("sophiasVariable", valueeeee);
+    storeValue("sophiasVariable", value);
   }
 })
 
-onNewValue("sophiasVariable", function(value) {
+onNewValue("sophiasVariable", function(e) {
   $("#value-output").html(value)
 })
+
+//function storeValue(name, value) {	
+//  var obj = {}
+//  obj[name] = value;
+//  store.child("vars").update(obj)
+//}
+//
+//function onNewValue(name, callback) {	
+//  store.child("vars").child(name).on("value", function(data) {		
+//    callback(data.val());
+//  })
+//}
