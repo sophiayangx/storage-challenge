a4519cf56fec0641392b48f5c63bb60accbbc1c8
sophiayangx <sopyang@nuevaschool.org>
2016-10-04 08:41:27 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var input = $("#not-a-real-id")
 
-input.keydwn(function(e) {	
+input.keydown(function(e) {	
   if (e.keyCode == "13") {
     var valueToSave = input.val();
     storeValue("davidsVariable", valueeeee);
